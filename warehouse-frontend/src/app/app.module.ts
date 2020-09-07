40575ec3210c73f4b02b2cad5c531d40d73c1c4f
goran-b <goran.b@mail.com>
2020-09-07 17:58:35 +0200

    added content and style

--- warehouse-frontend/src/app/app.module.ts
+++ warehouse-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserRecentPurchasesComponent } from './user-recent-purchases/user-recent-purchases.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { UserRecentPurchasesComponent } from './user-recent-purchases/user-recen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
